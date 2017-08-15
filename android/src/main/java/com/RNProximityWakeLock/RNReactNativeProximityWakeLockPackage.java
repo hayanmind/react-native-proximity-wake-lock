b01b42a8209b47c6deed9f12db9319ef12ddceb4
Daehoon Kim <ghsdh3409@gmail.com>
2017-08-15 11:42:32 +0900

    Deprecated createJSModules in 0.47

--- android/src/main/java/com/RNProximityWakeLock/RNReactNativeProximityWakeLockPackage.java
+++ android/src/main/java/com/RNProximityWakeLock/RNReactNativeProximityWakeLockPackage.java
@@ -16,6 +16,12 @@ public class RNReactNativeProximityWakeLockPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNReactNativeProximityWakeLockModule(reactContext));
     }
 
+    // Deprecated RN 0.47
+ +  // @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {		
+      return Collections.emptyList();		
+    }
+    
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
